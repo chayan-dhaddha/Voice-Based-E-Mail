4b685b7d34fa1eaead4316d2ee9b920a6cb417cf
Chayan Dhaddha <cdhaddha@gmail.com>
2017-05-31 16:22:17 +0530

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-pip.
+sudo -H pip2 install --upgrade pip
+sudo python -m pip install html2text
+sudo apt-get install git
+sudo apt-get install libasound-dev
+git clone http://people.csail.mit.edu/hubert/git/pyaudio.git
+cd pyaudio
+sudo python setup.py install
+sudo apt-get install libportaudio-dev
+sudo apt-get install python-dev
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev
+sudo pip install SpeechRecognition        
+sudo pip install pyttsx
+sudo pip install --upgrade google-api-python-client
